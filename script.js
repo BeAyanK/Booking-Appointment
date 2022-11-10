596d05be8d97fda61635b557bb6ec3519bda1389
Ayan Khan <beaayank@gmail.com>
2022-11-10 12:50:00 +0530

    Stringify and parse done

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 function onCall(e) {
-    // e.preventDefault();
-    // console.log(e.target.name.value);
-    // console.log(e.target.mail.value);
-    // console.log(e.target.phone.value);
-    // console.log(e.target.date.value);
-    // console.log(e.target.time.value);
+    // localStorage.setItem('UserName', e.target.name.value);
+    // localStorage.setItem('UserMail', e.target.mail.value);
+    // localStorage.setItem('UserPhone', e.target.phone.value);
+    // localStorage.setItem('UserDate', e.target.date.value);
+    // localStorage.setItem('UserTime', e.target.time.value);
 
-    // var obj = new Array;
-    // obj.push({'name':e.target.name.value});
-    // obj.push({'mail':e.target.mail.value});
-    // obj.push({'phone':e.target.phone.value});
-    // obj.push({'date':e.target.date.value});
-    // obj.push({'time':e.target.time.value});
+    var name = e.target.name.value;
+    var mail = e.target.mail.value;
+    var phone = e.target.phone.value;
+    var date = e.target.date.value;
+    var time = e.target.time.value;
 
-    // console.log(obj);
+    let user = {
+        name: name,
+        mail: mail,
+        phone: phone,
+        date: date,
+        time: time,
+    }
 
-    // localStorage.setItem('User', obj.);
+    let userStr = JSON.stringify(user);
 
-    localStorage.setItem('UserName', e.target.name.value);
-    localStorage.setItem('UserMail', e.target.mail.value);
-    localStorage.setItem('UserPhone', e.target.phone.value);
-    localStorage.setItem('UserDate', e.target.date.value);
-    localStorage.setItem('UserTime', e.target.time.value);
+    localStorage.setItem('User',userStr);
+
+    let userPar = JSON.parse(localStorage.getItem('User'));
+
+    console.log(userPar);
+    console.log(userStr);
 }
