b30ecc68d4b216ad2a63d95627a830b75383efd9
BeAyanK <beaayank@gmail.com>
2022-11-30 16:53:41 +0530

    Scaled and Fetchable

--- script.js
+++ script.js
@@ -1,30 +1,40 @@
 function onCall(e) {
-    // localStorage.setItem('UserName', e.target.name.value);
-    // localStorage.setItem('UserMail', e.target.mail.value);
-    // localStorage.setItem('UserPhone', e.target.phone.value);
-    // localStorage.setItem('UserDate', e.target.date.value);
-    // localStorage.setItem('UserTime', e.target.time.value);
+    e.preventDefault();
 
-    var name = e.target.name.value;
-    var mail = e.target.mail.value;
-    var phone = e.target.phone.value;
-    var date = e.target.date.value;
-    var time = e.target.time.value;
+    var name = document.getElementById('name').value;//e.target.name.value;
+    var mail = document.getElementById('mail').value; //e.target.mail.value;
+    var phone = document.getElementById('phone').value; //e.target.phone.value;
+    var date = document.getElementById('date').value; //e.target.date.value;
+    var time = document.getElementById('time').value; //e.target.time.value;
 
-    let user = {
+    var user = {
         name: name,
         mail: mail,
         phone: phone,
         date: date,
         time: time,
+    };
+
+
+    var userStr = JSON.stringify(user);
+
+    localStorage.setItem(mail, userStr);
+
+    var ul = document.querySelector('ul');
+    ul.style.border = 'solid 2px black';
+    for (var i = 0; i < localStorage.length; i++) {
+
+        var userPar = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        var li = document.createElement('li');
+
+        li.className = 'list-group-item';
+        li.appendChild(document.createTextNode(JSON.parse(localStorage.getItem(mail))));
+
+        li.innerHTML = (i + 1) + '. ' + userPar.name + ' ' + userPar.mail + " " + userPar.phone + ' ' + userPar.date + ' ' + userPar.time;
+        li.style.color = "black";
+        li.style.backgroundColor = '#f4f4f4';
+        ul.appendChild(li);
     }
 
-    let userStr = JSON.stringify(user);
-
-    localStorage.setItem('User',userStr);
-
-    let userPar = JSON.parse(localStorage.getItem('User'));
-
-    console.log(userPar);
-    console.log(userStr);
+    document.querySelector('form').reset();
 }
